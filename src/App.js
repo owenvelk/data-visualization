18cce294fa710ad216d0ee133679da9c1db3c00b
Owen Velkoff <owenv@lawn-143-215-48-179.lawn.gatech.edu>
2023-01-12 09:45:29 -0500

    modifying deployment to account for routes

--- src/App.js
+++ src/App.js
@@ -5,23 +5,20 @@ import Chart2 from './pages/Chart2';
 import Chart3 from './pages/Chart3';
 import Comparison from './pages/Comparison';
 import Data from './assets/frontend_intern_project_data';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import AppRoutes from './AppRoutes';
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
+      <AppRoutes />
         <Routes>
-          <Route path="/" element={<AppRoutes />}>
-            <Route index element={<Chart1 data={Data} />} />
-            <Route path="chart2" element={<Chart2 data={Data}/>} />
-            <Route path="chart3" element={<Chart3 data={Data}/>} />
-            <Route path="comp" element={<Comparison data={Data}/>} />
-            {/* <Route path="*" element={<NoPage />} /> */}
-          </Route>
+          <Route path="/" element={<Chart1 data={Data} />} />
+          <Route path="chart2" element={<Chart2 data={Data}/>} />
+          <Route path="chart3" element={<Chart3 data={Data}/>} />
+          <Route path="comp" element={<Comparison data={Data}/>} />
+          {/* <Route path="*" element={<NoPage />} /> */}
         </Routes>
-      </BrowserRouter>
     </div>
   );
 }
